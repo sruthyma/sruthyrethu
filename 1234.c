0bcbe2f6d98966ed7e2d4c91354d3aa557f04780
sruthyma <sruthyma97@gmail.com>
2017-09-14 14:55:12 +0530

    Create 1234.c

---
+++ 1234.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int a;
+printf("enter the  number:"\n);
+scanf("%d",&a);
+if(a/2==0)
+{
+printf("\n The num is even");
+}
+else
+{
+printf("\n the num is odd");
+}
+return 0;
+}
